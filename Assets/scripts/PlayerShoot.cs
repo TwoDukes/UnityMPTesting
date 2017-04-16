56e1af705a7186be20a638233e477aa09494e2d5
magicdustin <magicdustin14@gmail.com>
2017-04-16 14:29:38 -0700

    Added adjustable fire rate to weapons and set up system for multiple weapon types

--- Assets/scripts/PlayerShoot.cs
+++ Assets/scripts/PlayerShoot.cs
@@ -1,17 +1,14 @@
 ﻿using UnityEngine.Networking;
 using UnityEngine;
 
+[RequireComponent(typeof(WeaponManager))]
 public class PlayerShoot : NetworkBehaviour {
 
     private const string PLAYER_TAG = "Player";
 
-    [SerializeField]
-    private PlayerWeapon weapon;
+    private WeaponManager weaponManager;
 
-    [SerializeField]
-    private GameObject weaponGFX;
-    [SerializeField]
-    private string weaponLayerName = "Weapon";
+    private PlayerWeapon currentWeapon;
 
     [SerializeField]
     private Camera cam;
@@ -21,32 +18,49 @@ public class PlayerShoot : NetworkBehaviour {
 
     private void Start()
     {
+        weaponManager = GetComponent<WeaponManager>();
+
         if(cam == null)
         {
             Debug.LogError("PlayerShoot: No camera referenced");
             enabled = false;
         }
-
-        weaponGFX.layer = LayerMask.NameToLayer(weaponLayerName); //Sets gun to weapon layer to be rendered by weapon camera only
     }
 
     private void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        currentWeapon = weaponManager.GetCurrentWeapon();
+
+
+        if (currentWeapon.fireRate <= 0)
         {
-            Shoot();
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
         }
+        else
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                InvokeRepeating("Shoot", 0f, 1f / currentWeapon.fireRate);
+            } else if(Input.GetButtonUp("Fire1"))
+            {
+                CancelInvoke("Shoot");
+            }
+        }
+        
     }
     
     [Client]
     private void Shoot()
     {
         RaycastHit _hit;
-        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, weapon.range, mask)) //if we hit something
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, currentWeapon.range, mask)) //if we hit something
         {
             if (_hit.collider.tag == PLAYER_TAG)
             {
-                CmdPlayerShot(_hit.collider.name, weapon.damage);
+                CmdPlayerShot(_hit.collider.name, currentWeapon.damage);
             }
         }
     }
